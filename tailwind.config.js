31156a95f51034ec145521a830105e352fb0593b
gigahex <33027225-gigahex@users.noreply.replit.com>
2025-05-06 13:50:33 +0000

    Restored to 'c006efc66fdfd208f7a6b084f2c0a79017cb59b4'
    Replit-Restored-To: c006efc66fdfd208f7a6b084f2c0a79017cb59b4

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
